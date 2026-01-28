bb412e676933e4cc1b4d4a6ce30018a8ae7263b1
avneetsingh90 <avneet_singh90@ymail.com>
2026-01-28 11:23:06 +0530

    Sync

--- SmartServe.Domain/Constants/Constants.cs
+++ SmartServe.Domain/Constants/Constants.cs
@@ -6,6 +6,7 @@
 		public const string DataValidationError = "R01";
 		public const string Error = "R03";
 		public const string DuplicateNotAllowed = "R04";
+		public const string ActiveOrderExists = "R05";
 	}
 	public static class ResultMessages
 	{
@@ -13,6 +14,7 @@
 		public const string DataValidationError = "Data Validation Error";
 		public const string Error = "Technical Error";
 		public const string DuplicateNotAllowed = "Duplicate Not Allowed";
+		public const string ActiveOrderExists = "Active Order Exists";
 	}
 	public static class TableStatusCodes
 	{
